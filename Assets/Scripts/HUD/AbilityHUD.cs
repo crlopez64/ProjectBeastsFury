ec71502b8d3e577fe23ae07d84a6a1220d0389c9
crlopez64 <57076460+crlopez64@users.noreply.github.com>
2021-08-24 19:15:08 -0500

    Delete Assets/Scripts directory

--- Assets/Scripts/HUD/AbilityHUD.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-/// <summary>
-/// Script keeping track of any Ability Text visuals.
-/// </summary>
-public class AbilityHUD : MonoBehaviour
-{
-    public AbilityText[] abilities;
-}
